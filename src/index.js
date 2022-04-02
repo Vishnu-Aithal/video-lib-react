3bac1162995d5ca65a673473986068790758ff71
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-04-02 21:08:29 +0530

    Initial Setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 
 // Call make Server
 makeServer();
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+root.render(<App />);
