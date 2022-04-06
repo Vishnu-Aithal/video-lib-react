52bf158be5fcab67ed155277dc4c28454ea0c552
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-04-07 01:14:48 +0530

    add - card drop down

--- src/components/CardDropDown.js
+++ src/components/CardDropDown.js
@@ -1,16 +1,93 @@
-const CardDropDownMenu = ({
-    inWatchLater = false,
-    addToPlaylist = () => {},
-    addToWatchLater = () => {},
-}) => {
+import { openPlaylistsModal } from "utility-functions/playlistHandler";
+import {
+    addToWatchlater,
+    inWatchlater,
+    removeFromWatchlater,
+} from "utility-functions/userHandler";
+import { useRef, useEffect } from "react";
+import { useAuth } from "contexts/auth-context";
+import { useUserData } from "contexts/user-context";
+import { useLoader } from "contexts/loader-context";
+import { usePlaylists } from "contexts/playlist-context";
+import { useNavigate, useLocation } from "react-router-dom";
+export const CardDropDown = ({ dropDown, setDropDown, video }) => {
+    const {
+        userState: { watchlater },
+        userDispatch,
+    } = useUserData();
+    const {
+        authState: { token, isLoggedIn },
+    } = useAuth();
+    const { showLoader, hideLoader } = useLoader();
+    const { playlistsDispatch } = usePlaylists();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const ref = useRef();
+    useEffect(() => ref.current.focus(), []);
+
     return (
-        <ul className="list-group">
-            <li className="list__item" onClick={addToPlaylist}>
+        <ul
+            tabIndex="-1"
+            ref={ref}
+            onBlur={() => setDropDown(false)}
+            className={`card__drop-down list-group shadow-sm br-1 ${
+                dropDown ? "show" : ""
+            }`}>
+            <li
+                className="list__item bg-white"
+                onClick={() => {
+                    isLoggedIn
+                        ? openPlaylistsModal(video, playlistsDispatch)
+                        : navigate("/sign-in", {
+                              state: {
+                                  from: location.pathname,
+                              },
+                          });
+                    setDropDown(false);
+                }}>
                 Add to Playlist
             </li>
-            {!inWatchLater && (
-                <li className="list__item" onClick={addToWatchLater}>
+            {!inWatchlater(watchlater, video) ? (
+                <li
+                    className="list__item bg-white"
+                    onClick={() => {
+                        isLoggedIn
+                            ? addToWatchlater(
+                                  token,
+                                  video,
+                                  userDispatch,
+                                  showLoader,
+                                  hideLoader
+                              )
+                            : navigate("/sign-in", {
+                                  state: {
+                                      from: location.pathname,
+                                  },
+                              });
+                        setDropDown(false);
+                    }}>
                     Add to Watch Later
+                </li>
+            ) : (
+                <li
+                    className="list__item bg-white"
+                    onClick={() => {
+                        isLoggedIn
+                            ? removeFromWatchlater(
+                                  token,
+                                  video,
+                                  userDispatch,
+                                  showLoader,
+                                  hideLoader
+                              )
+                            : navigate("/sign-in", {
+                                  state: {
+                                      from: location.pathname,
+                                  },
+                              });
+                        setDropDown(false);
+                    }}>
+                    Remove From Watch Later
                 </li>
             )}
         </ul>
