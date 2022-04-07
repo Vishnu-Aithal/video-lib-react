6c248665d47ba95b92624fa79916b7355fe58426
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-04-07 22:14:54 +0530

    fix - 404 page

--- src/routes/NotFound.js
+++ src/routes/NotFound.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 export const NotFound = () => {
     return (
-        <div>
+        <div className="shadow-md p-6 w-75p mx-auto mt-6 bg-primary-light">
             <h1 className="heading-xl text-center mt-6 clr-red">
                 404 Not Found
             </h1>
