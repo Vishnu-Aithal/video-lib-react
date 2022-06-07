acf77beed47681760590fa204e42a004895ec8d4
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-06-07 10:47:32 +0530

    add typescript and rename all files

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,9 @@ import { PlaylistProvider } from "contexts/playlist-context";
 // Call make Server
 makeServer();
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLDivElement
+);
 
 root.render(
     <ContextProvider
